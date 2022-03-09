4d22cd63a2dfe3cd1e31ab4d8fc5e8ab19197bfe
David Medina <dmedi@DAVID.DAVID>
2022-03-09 12:59:31 -0600

    cambio1

--- shortcuts/scripts/cal.js
+++ shortcuts/scripts/cal.js
@@ -1,21 +1,138 @@
-$('body').on("keydown", function(e) {
+
+////////////////////////////
+const calculator = {
+    displayValue: '',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
+
+function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else {
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+}
+
+
+function inputDecimal(dot) {
+    // If the `displayValue` does not contain a decimal point
+    if (!calculator.displayValue.includes(dot)) {
+        // Append the decimal point
+        calculator.displayValue += dot;
+    }
+}
+
+function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator
+    const inputValue = parseFloat(displayValue);
+
+    if (operator && calculator.waitingForSecondOperand) {
+        calculator.operator = nextOperator;
+        return;
+    }
+
+    if (firstOperand == null) {
+        calculator.firstOperand = inputValue;
+    } else if (operator) {
+        const currentValue = firstOperand || 0;
+        const result = performCalculation[operator](currentValue, inputValue);
+
+        calculator.displayValue = String(result);
+        calculator.firstOperand = result;
+    }
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+}
+
+const performCalculation = {
+    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+
+    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+
+    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+
+    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+
+    '=': (firstOperand, secondOperand) => secondOperand
+};
+
+function resetCalculator() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+}
+
+function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+    const { target } = event;
+    if (!target.matches('button')) {
+        return;
+    }
+
+    if (target.classList.contains('operator')) {
+        handleOperator(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('all-clear')) {
+        resetCalculator();
+        updateDisplay();
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();
+});
+
+$('body').on("keyup", function numeros(e) {
+
     console.log(e);
-    var cal = document.getElementById("print");
-    /*if (e.keyCode === 67) {
 
-        cal.focus();
-    }*/
 
-    if (e.keyCode === 96) { //imprime 0
-        cal.focus();
-    }
-})
 
-$('button').on("click", function numeros(e) {
+    const arreglo = {
+        13: "=",  
+        107: "+",  
+        109: "-",
+        106: "*", 
+        111: "/", 
+        
+        
+        96: 0,
+        97: 1,
+        98: 2, 
+        51: 3, 
+        100: 4, 
+        101: 5, 
+        102: 6, 
+        103: 7, 
+        104: 8, 
+        105: 9, 
+    };
 
-    if (e.keyCode === 103) { //imprime 0
-        cal.focus();
-    }
+    var valor = arreglo[e.keyCode];
 
+    $("button[value= '${valor}' ]").trigger("click");
 
 })
--- shortcuts/scripts/index.js
+++ shortcuts/scripts/index.js
@@ -1,5 +1,6 @@
 $('button').on("keydown", function shortcuts(e) {
-    if (e.keyCode === 67) {
+
+    if (e.keyCode === 67) { //Si aprieta el keyCode 67 (letra c)
 
         window.open("calculadora/cal.html");
         e.preventDefault();
