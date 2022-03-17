e44acb5ff4917100a981ea644982b5f5fa40eeb2
David Medina <dmedi@DAVID.DAVID>
2022-03-17 13:10:21 -0600

    calculadora cambio 2

--- shortcuts/scripts/cal2.js
+++ shortcuts/scripts/cal2.js
@@ -1,16 +1,25 @@
-var num1 = [];
-var num2 = [];
+/* var num1= "";
+var num2= "";
+var validar = false;
 var operator;
 var display = document.getElementById("pantalla");
 
 function number(num)
 {
-    if (num === "+" || num === "-" || num === "*" || num === "/")
+    if (validar)
     {
-        display.value = "0";
-        num2 = num2 + num;
-        display.value= num2;
-        console.log("num2 = " + num2);
+        if (num == "+" || num == "-" || num == "*" || num == "/")
+        {
+            operator = num;//almacenar el operador
+        }
+        else
+        {
+            //display.value = "0";
+            num2 = num2 + num;
+            display.value= num2;
+            console.log("num2 = " + num2);
+        }
+           
     }
     else
     {
@@ -18,18 +27,97 @@ function number(num)
         display.value= num1;
         console.log("num1 = " + num1);
     }
-    
-    
+
+}
+
+function operations(num1,num2, operator)
+{
+    var result;
+
+    num1 = parseFloat(num1); 
+    num2 = parseFloat(num2);
+
+    switch(operator)
+    {
+        case "+":
+            result = num1 + num2;
+            display.value = result;
+            validar = false;
+            break;
+
+        case "-":
+            result = num1 - num2;
+            display.value = result;
+            validar = false;
+            break;
+
+        case "*":
+            result = num1 * num2;
+            display.value = result;
+            validar = false;
+        break;
+
+        case "/":
+            result = num1 / num2;
+            display.value = result;
+            validar = false;
+            break;
+    }
+
 }
 
 $('button').on('click', function (e){
 
     const { target } = e;
 
-    if ( target.classList.contains('number') || target.classList.contains('operator'))
+    if ( target.classList.contains('number'))
     {
-        number(target); 
+        number(target.value); //agregar el primer número
+    }
+
+    if (target.classList.contains('operator'))
+    {
+        if (num1 == "0") //validar si quiere un operador al principio
+        {
+            number(target.value);
+        }else{
+            validar = true;
+            number(target.value);//agregar el segundo número
+        }
+        
+        
+    }
+
+    if (target.classList.contains('equal-sign'))
+    {
+        operations(num1, num2, operator);
     }
     
-    
+}) */
+
+const operation = {
+    leftValue: '',
+    operator: '',
+    rightValue: 0
+}
+
+var display = document.getElementById("pantalla");
+
+function numberValue(num)
+{
+    operation.leftValue = operation.leftValue + num;
+
+    display.value = operation.leftValue;
+    console.log(operation.leftValue);
+}
+
+$('button').on('click', function (e){
+
+    const { target } = e;
+
+    if (target.classList.contains('number'))
+    {
+        numberValue(target.value);
+    }
+
 })
